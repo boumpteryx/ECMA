bd158d3571ce9345066fe1e8c42dca5050ef950b
Antoine Desjardins <antoine.desjardins@eleves.enpc.fr>
2022-02-06 21:24:11 +0100

    automatic

--- main.jl
+++ main.jl
@@ -10,7 +10,7 @@ start = time()
 # dualisation("20_USA-road-d.BAY.gr")
 
 ### resolution par plans coupants
-plans_coupants("20_USA-road-d.BAY.gr")
+# plans_coupants("40_USA-road-d.COL.gr")
 
 ### resolution par LazyCallback (branch-and-cut)
 # branch_and_cut("20_USA-road-d.COL.gr")
@@ -19,11 +19,18 @@ plans_coupants("20_USA-road-d.BAY.gr")
 # heuristique("20_USA-road-d.BAY.gr")
 
 
-
-computation_time = time() - start
-println(computation_time)
-
-
-# for file in readdir("./Instances_ECMA")
-  # println(file)
-# end
+files_and_dirs = readdir("./temporaire")
+p = Vector{Char}(undef,0)
+secs = Vector{Float64}(undef,0)
+for file in files_and_dirs
+    actual_time = time()
+    println("filename = ", file)
+    result = branch_and_cut(file)
+    println("filename = ", file)
+    append!(p, file)
+    local computation_time = time() - actual_time
+    append!(secs, computation_time)
+    println(computation_time, " seconds")
+end
+println(p)
+println(secs)
